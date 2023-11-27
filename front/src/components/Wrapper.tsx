f5856430161a87bfd61a32a643834d7e98507a82
Kacper <275961@student.pwr.edu.pl>
2023-11-27 16:11:21 +0100

    fix: Stylowanie bloku wykladow

--- front/src/components/Wrapper.tsx
+++ front/src/components/Wrapper.tsx
@@ -1,6 +1,7 @@
 import { useAuth } from './Auth'
 import Login from './Login'
 import Panel from './Panel'
+import omatkoLogo from '../assets/omatko logo.jpg'
 
 
 
@@ -10,7 +11,7 @@ export const Wrapper: React.FC = () => {
     return (
         <div>
             <div className="img-box">
-            <img src="/src/assets/omatko logo.jpg" alt="Logo OMatKo"/>
+            <img src={omatkoLogo} alt="Logo OMatKo"/>
             </div>
             { state.isAuntheticated ? <Panel/> : <Login/>}
         </div>
