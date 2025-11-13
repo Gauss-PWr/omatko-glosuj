fd2d72319b4dc1144fe5053df79d8b3f2fbaa6f5
fxGRDN <kacypacy@gmail.com>
2025-11-13 14:15:51 +0100

    feat: global loading and spiner

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,11 +4,14 @@ import { Provider } from "react-redux";
 import "./index.css";
 import App from "@/App";
 import { store } from "@/store";
+import { LoadingProvider } from "./providers/LoadingProvider";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <LoadingProvider>
+        <App />
+      </LoadingProvider>
     </Provider>
   </StrictMode>
 );
